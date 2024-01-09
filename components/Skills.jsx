6a9106c8b7bd19411271c1a2d2e9a1ab5c4f6ff0
nazaadimoha <nazaadimoha98@gmail.com>
2024-01-09 14:08:30 +0100

    added more skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,12 +9,30 @@ import TAILWIND from "../public/assets/skills/tailwind.png";
 import FIREBASE from "../public/assets/skills/firebase.png";
 import NODE from "../public/assets/skills/node.png";
 import MONGO from "../public/assets/skills/mongo.png";
+import REACTQUERY from "../public/assets/skills/react-query-icon.svg";
+import REDUX from "../public/assets/skills/redux-logo.svg";
+import TYPESCRIPT from "../public/assets/skills/typescript.svg";
+import REACTNATIVE from "../public/assets/skills/react-native-1.svg";
+import { motion } from "framer-motion";
 
 
 const Skills = () => {
+  //animate on scroll for this component using framer motion
+
   return (
-    <div id="skills" className="w-full lg:h-screen p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+    <motion.div
+     id="skills" className="w-full lg:h-screen p-2"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2.5 }}
+    >
+      <motion.div
+        className="max-w-[1240px] mx-auto flex flex-col justify-center h-full"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2.5 }}
+
+      >
         <p className="text-3xl tracking-widest uppercase text-[#5651e5] font-bold">
           Skills
         </p>
@@ -108,7 +126,7 @@ const Skills = () => {
               </div>
 
               <div className="flex flex-col items-center justify-center">
-                <h3>NEXTJS</h3>
+                <h3>NEXT.JS</h3>
               </div>
 
             
@@ -190,9 +208,85 @@ const Skills = () => {
             
             </div>
           </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={REDUX}
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>REDUX</h3>
+              </div>
+
+            
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={REACTQUERY}
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tanstack Query</h3>
+              </div>
+
+            
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={TYPESCRIPT}
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>TYPESCRIPT</h3>
+              </div>
+
+            
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={REACTNATIVE}
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>REACT NATIVE</h3>
+              </div>
+
+            
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
